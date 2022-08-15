3795ca86c65b4c0c560279d98268761aebdfc1eb
chirag2103 <67541093+chirag2103@users.noreply.github.com>
2022-08-15 21:51:56 +0530

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+module.exports = {
+    products:[
+        {
+            _id: '1',
+            name: 'Full Zip Running Shirts',
+            category: 'Shirts',
+            image: './img/img1.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStocks: 6.
+        },
+        {
+            _id: '2',
+            name: 'Full Zip Running Shirts',
+            category: 'Shirts',
+            image: './img/img2.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStocks: 6.
+        },
+        {
+            _id: '3',
+            name: 'Full Zip Running Shirts',
+            category: 'Shirts',
+            image: './img/img3.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStocks: 6.
+        },
+        {
+            _id: '4',
+            name: 'Full Zip Running Shirts',
+            category: 'Shirts',
+            image: './img/img4.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStocks: 6.
+        },
+        {
+            _id: '5',
+            name: 'Full Zip Running Shirts',
+            category: 'Shirts',
+            image: './img/img5.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStocks: 6.
+        },
+        {
+            _id: '6',
+            name: 'Full Zip Running Shirts',
+            category: 'Shirts',
+            image: './img/img6.jpg',
+            price: 60,
+            brand: 'Hiskywin',
+            rating: 4.5,
+            numReviews: 10,
+            countInStocks: 6.
+        },
+    ],
+}
